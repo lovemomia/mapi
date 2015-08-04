d649fc1f208b8825bf790381e70591accba04d3c
吴秋锋 <arthas.wuqf@gmail.com>
2015-08-04 14:48:55 +0800

    validation

--- src/main/java/cn/momia/mapi/api/v1/FeedbackV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/FeedbackV1Api.java
@@ -3,6 +3,7 @@ package cn.momia.mapi.api.v1;
 import cn.momia.mapi.common.http.MomiaHttpParamBuilder;
 import cn.momia.mapi.common.http.MomiaHttpRequest;
 import cn.momia.mapi.web.response.ResponseMessage;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class FeedbackV1Api extends AbstractV1Api {
     @RequestMapping(method = RequestMethod.POST)
     public ResponseMessage addFeedback(@RequestParam String content, @RequestParam String email) {
+        if (StringUtils.isBlank(content) || StringUtils.isBlank(email)) return ResponseMessage.BAD_REQUEST;
+
         MomiaHttpParamBuilder builder = new MomiaHttpParamBuilder()
                 .add("content", content)
                 .add("email", email);
