dce4520a90349e18b7c43dcf434408988b9f60bc
吴秋锋 <arthas.wuqf@gmail.com>
2015-09-16 18:17:31 +0800

    im service

---
+++ src/main/java/cn/momia/mapi/api/v1/ImV1Api.java
@@ -0,0 +1,17 @@
+package cn.momia.mapi.api.v1;
+
+import cn.momia.api.im.ImServiceApi;
+import cn.momia.common.api.http.MomiaHttpResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/im")
+public class ImV1Api {
+    @RequestMapping(value = "/token", method = RequestMethod.POST)
+    public MomiaHttpResponse token(@RequestParam String utoken) {
+        return MomiaHttpResponse.SUCCESS(ImServiceApi.IM.getToken(utoken));
+    }
+}
