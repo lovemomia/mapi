20fa9e02dd48f1ce6c5834d5731f9f596dba78b3
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-12 17:39:41 +0800

    完成课程详情接口

--- src/main/java/cn/momia/mapi/api/v1/AbstractV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/AbstractV1Api.java
@@ -1,5 +1,6 @@
 package cn.momia.mapi.api.v1;
 
+import cn.momia.api.course.dto.CourseBookDto;
 import cn.momia.api.course.dto.CourseDto;
 import cn.momia.api.course.dto.SubjectDto;
 import cn.momia.common.api.dto.PagedList;
@@ -7,24 +8,74 @@ import cn.momia.image.api.ImageFile;
 import cn.momia.mapi.api.AbstractApi;
 import cn.momia.api.user.dto.UserDto;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class AbstractV1Api extends AbstractApi {
+    private void processLargeImgs(List<String> imgs) {
+        if (imgs == null) return;
+
+        for (int i = 0; i < imgs.size(); i++) {
+            imgs.set(i, ImageFile.largeUrl(imgs.get(i)));
+        }
+    }
+
+    private void processMiddleImgs(List<String> imgs) {
+        if (imgs == null) return;
+
+        for (int i = 0; i < imgs.size(); i++) {
+            imgs.set(i, ImageFile.middleUrl(imgs.get(i)));
+        }
+    }
+
+    private void processSmallImgs(List<String> imgs) {
+        if (imgs == null) return;
+
+        for (int i = 0; i < imgs.size(); i++) {
+            imgs.set(i, ImageFile.smallUrl(imgs.get(i)));
+        }
+    }
+
     protected PagedList<CourseDto> processPagedCourses(PagedList<CourseDto> courses) {
         for (CourseDto course : courses.getList()) {
-            course.setCover(ImageFile.largeUrl(course.getCover()));
+            processCourse(course);
         }
 
         return courses;
     }
 
-    protected SubjectDto processSubject(SubjectDto subjectDto) {
-        List<String> imgs = subjectDto.getImgs();
+    protected CourseDto processCourse(CourseDto course) {
+        course.setCover(ImageFile.largeUrl(course.getCover()));
+
+        processLargeImgs(course.getImgs());
+        processCourseBook(course.getBook());
+
+        return course;
+    }
+
+    private CourseBookDto processCourseBook(CourseBookDto book) {
+        if (book == null) return null;
+
+        List<String> imgs = new ArrayList<String>();
+        List<String> largeImgs = new ArrayList<String>();
+        for (String img : book.getImgs()) {
+            imgs.add(ImageFile.smallUrl(img));
+            largeImgs.add(ImageFile.largeUrl(img));
+        }
+
+        book.setImgs(imgs);
+        book.setLargeImgs(largeImgs);
+
+        return book;
+    }
+
+    protected SubjectDto processSubject(SubjectDto subject) {
+        List<String> imgs = subject.getImgs();
         for (int i = 0; i < imgs.size(); i++) {
             imgs.set(i, ImageFile.largeUrl(imgs.get(i)));
         }
 
-        return subjectDto;
+        return subject;
     }
 
     protected UserDto processUser(UserDto user) {
---
+++ src/main/java/cn/momia/mapi/api/v1/course/CourseV1Api.java
@@ -0,0 +1,21 @@
+package cn.momia.mapi.api.v1.course;
+
+import cn.momia.api.course.CourseServiceApi;
+import cn.momia.common.api.http.MomiaHttpResponse;
+import cn.momia.mapi.api.v1.AbstractV1Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/course")
+public class CourseV1Api extends AbstractV1Api {
+    @Autowired private CourseServiceApi courseServiceApi;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public MomiaHttpResponse get(@RequestParam long id) {
+        return MomiaHttpResponse.SUCCESS(processCourse(courseServiceApi.get(id)));
+    }
+}
