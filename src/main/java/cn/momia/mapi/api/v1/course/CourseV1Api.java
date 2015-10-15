b7f81c96b956fcfe917d63d827d0dec8f96d79a8
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-15 19:40:23 +0800

    收藏

--- src/main/java/cn/momia/mapi/api/v1/course/CourseV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/course/CourseV1Api.java
@@ -1,8 +1,12 @@
 package cn.momia.mapi.api.v1.course;
 
 import cn.momia.api.course.CourseServiceApi;
+import cn.momia.api.course.dto.CourseDto;
+import cn.momia.api.user.UserServiceApi;
+import cn.momia.api.user.dto.UserDto;
 import cn.momia.common.api.http.MomiaHttpResponse;
 import cn.momia.mapi.api.v1.AbstractV1Api;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,10 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/course")
 public class CourseV1Api extends AbstractV1Api {
     @Autowired private CourseServiceApi courseServiceApi;
+    @Autowired private UserServiceApi userServiceApi;
 
     @RequestMapping(method = RequestMethod.GET)
-    public MomiaHttpResponse get(@RequestParam long id) {
-        return MomiaHttpResponse.SUCCESS(processCourse(courseServiceApi.get(id)));
+    public MomiaHttpResponse get(@RequestParam(required = false, defaultValue = "") String utoken, @RequestParam long id) {
+        CourseDto course = courseServiceApi.get(id);
+        if (!StringUtils.isBlank(utoken)) {
+            UserDto user = userServiceApi.get(utoken);
+            course.setFavored(courseServiceApi.isFavored(user.getId(), id));
+        }
+
+        return MomiaHttpResponse.SUCCESS(processCourse(course));
     }
 
     @RequestMapping(value = "/sku/week", method = RequestMethod.GET)
@@ -38,4 +49,18 @@ public class CourseV1Api extends AbstractV1Api {
 //    public MomiaHttpResponse booking(@RequestParam String utoken, @RequestParam() String order) {
 //        return MomiaHttpResponse.SUCCESS(courseServiceApi.booking());
 //    }
+
+    @RequestMapping(value = "/favor", method = RequestMethod.POST)
+    public MomiaHttpResponse favor(@RequestParam String utoken, @RequestParam long id) {
+        UserDto user = userServiceApi.get(utoken);
+        if (!courseServiceApi.favor(user.getId(), id)) return MomiaHttpResponse.FAILED("添加收藏失败");
+        return MomiaHttpResponse.SUCCESS;
+    }
+
+    @RequestMapping(value = "/unfavor", method = RequestMethod.POST)
+    public MomiaHttpResponse unfavor(@RequestParam String utoken, @RequestParam long id) {
+        UserDto user = userServiceApi.get(utoken);
+        if (!courseServiceApi.unfavor(user.getId(), id)) return MomiaHttpResponse.FAILED("取消收藏失败");
+        return MomiaHttpResponse.SUCCESS;
+    }
 }
