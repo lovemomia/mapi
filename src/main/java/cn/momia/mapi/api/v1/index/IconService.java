d059b145ebd210f00252ce812b8648269fdc39c5
吴秋锋 <arthas.wuqf@gmail.com>
2015-10-19 19:53:42 +0800

    fix bug

--- src/main/java/cn/momia/mapi/api/v1/index/IconService.java
+++ src/main/java/cn/momia/mapi/api/v1/index/IconService.java
@@ -1,29 +1,12 @@
 package cn.momia.mapi.api.v1.index;
 
 import cn.momia.common.service.DbAccessService;
-import org.springframework.jdbc.core.RowCallbackHandler;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class IconService extends DbAccessService {
     public List<Icon> list(int cityId) {
-        final List<Icon> icons = new ArrayList<Icon>();
         String sql = "SELECT Title, Img, Action FROM SG_Icon WHERE (CityId=? OR CityId=0) AND Status=1 ORDER BY Weight DESC, AddTime DESC";
-        jdbcTemplate.query(sql, new Object[] { cityId }, new RowCallbackHandler() {
-            @Override
-            public void processRow(ResultSet rs) throws SQLException {
-                Icon icon = new Icon();
-                icon.setTitle(rs.getString("Title"));
-                icon.setImg(rs.getString("Img"));
-                icon.setAction(rs.getString("Action"));
-
-                icons.add(icon);
-            }
-        });
-
-        return icons;
+        return queryList(sql, new Object[] { cityId }, Icon.class);
     }
 }
