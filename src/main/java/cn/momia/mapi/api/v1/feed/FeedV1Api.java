d767e783865c69073f02d4327a89c4aaae3bdba7
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-04 13:03:03 +0800

    feed

---
+++ src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
@@ -0,0 +1,45 @@
+package cn.momia.mapi.api.v1.feed;
+
+import cn.momia.api.feed.FeedServiceApi;
+import cn.momia.api.feed.dto.FeedDto;
+import cn.momia.api.user.UserServiceApi;
+import cn.momia.api.user.dto.UserDto;
+import cn.momia.common.api.dto.PagedList;
+import cn.momia.common.api.http.MomiaHttpResponse;
+import cn.momia.common.webapp.config.Configuration;
+import cn.momia.mapi.api.v1.AbstractV1Api;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/feed")
+public class FeedV1Api extends AbstractV1Api {
+    @Autowired private FeedServiceApi feedServiceApi;
+    @Autowired private UserServiceApi userServiceApi;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public MomiaHttpResponse list(@RequestParam(required = false, defaultValue = "") String utoken, @RequestParam int start) {
+        if (start < 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        long userId = 0;
+        if (!StringUtils.isBlank(utoken)) {
+            UserDto user = userServiceApi.get(utoken);
+            userId = user.getId();
+        }
+
+        PagedList<FeedDto> pagedFeeds = feedServiceApi.list(userId, start, Configuration.getInt("PageSize.Feed"));
+        processFeeds(pagedFeeds.getList());
+
+        return MomiaHttpResponse.SUCCESS(pagedFeeds);
+    }
+
+    private void processFeeds(List<FeedDto> pagedFeedsList) {
+
+    }
+}
