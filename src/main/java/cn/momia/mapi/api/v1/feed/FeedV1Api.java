1c0321a13f15730ce6baae2b871673d06cd51625
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-05 16:35:56 +0800

    feed 加上subjectId

--- src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
@@ -90,6 +90,9 @@ public class FeedV1Api extends AbstractV1Api {
         JSONObject feedJson = JSON.parseObject(feed);
         feedJson.put("userId", user.getId());
 
+        CourseDto course = courseServiceApi.get(feedJson.getLong("courseId"), CourseDto.Type.BASE);
+        feedJson.put("subjectId", course.getSubjectId());
+
         if (!courseServiceApi.finished(user.getId(), feedJson.getLong("courseId"))) return MomiaHttpResponse.FAILED("发表Feed失败，所选课程不存在或您还没参加该课程");
         feedServiceApi.add(feedJson);
 
