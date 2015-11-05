6baa6547256689d1a39f83b38d60d8a1316881c7
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-05 09:47:33 +0800

    feed star/comment

--- src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
@@ -1,6 +1,7 @@
 package cn.momia.mapi.api.v1.feed;
 
 import cn.momia.api.feed.FeedServiceApi;
+import cn.momia.api.feed.dto.FeedCommentDto;
 import cn.momia.api.feed.dto.FeedDto;
 import cn.momia.api.user.UserServiceApi;
 import cn.momia.api.user.dto.UserDto;
@@ -56,6 +57,66 @@ public class FeedV1Api extends AbstractV1Api {
 
         UserDto user = userServiceApi.get(utoken);
         if (!feedServiceApi.delete(user.getId(), id)) return MomiaHttpResponse.FAILED("删除Feed失败");
+
+        return MomiaHttpResponse.SUCCESS;
+    }
+
+    @RequestMapping(value = "/comment", method = RequestMethod.GET)
+    public MomiaHttpResponse listComments(@RequestParam long id, @RequestParam int start) {
+        if (id <= 0 || start < 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        PagedList<FeedCommentDto> pagedComments = feedServiceApi.listComments(id, start, Configuration.getInt("PageSize.Feed.Comment"));
+        processComments(pagedComments.getList());
+
+        return MomiaHttpResponse.SUCCESS(pagedComments);
+    }
+
+    private void processComments(List<FeedCommentDto> comments) {
+        for (FeedCommentDto comment : comments) {
+            comment.setAvatar(ImageFile.smallUrl(comment.getAvatar()));
+        }
+    }
+
+    @RequestMapping(value = "/comment/add", method = RequestMethod.POST)
+    public MomiaHttpResponse addComment(@RequestParam String utoken, @RequestParam long id, @RequestParam String content) {
+        if (StringUtils.isBlank(utoken)) return MomiaHttpResponse.TOKEN_EXPIRED;
+        if (id <= 0 || StringUtils.isBlank(content)) return MomiaHttpResponse.BAD_REQUEST;
+
+        UserDto user = userServiceApi.get(utoken);
+        feedServiceApi.addComment(user.getId(), id, content);
+
+        return MomiaHttpResponse.SUCCESS;
+    }
+
+    @RequestMapping(value = "/comment/delete", method = RequestMethod.POST)
+    public MomiaHttpResponse deleteComment(@RequestParam String utoken, @RequestParam long id, @RequestParam(value = "cmid") long commentId) {
+        if (StringUtils.isBlank(utoken)) return MomiaHttpResponse.TOKEN_EXPIRED;
+        if (id <= 0 || commentId <= 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        UserDto user = userServiceApi.get(utoken);
+        feedServiceApi.deleteComment(user.getId(), id, commentId);
+
+        return MomiaHttpResponse.SUCCESS;
+    }
+
+    @RequestMapping(value = "/star", method = RequestMethod.POST)
+    public MomiaHttpResponse star(@RequestParam String utoken, @RequestParam long id) {
+        if (StringUtils.isBlank(utoken)) return MomiaHttpResponse.TOKEN_EXPIRED;
+        if (id <= 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        UserDto user = userServiceApi.get(utoken);
+        feedServiceApi.star(user.getId(), id);
+
+        return MomiaHttpResponse.SUCCESS;
+    }
+
+    @RequestMapping(value = "/unstar", method = RequestMethod.POST)
+    public MomiaHttpResponse unstar(@RequestParam String utoken, @RequestParam long id) {
+        if (StringUtils.isBlank(utoken)) return MomiaHttpResponse.TOKEN_EXPIRED;
+        if (id <= 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        UserDto user = userServiceApi.get(utoken);
+        feedServiceApi.unstar(user.getId(), id);
 
         return MomiaHttpResponse.SUCCESS;
     }
