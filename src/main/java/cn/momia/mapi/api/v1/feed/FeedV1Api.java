e0dd150d4cdb4f8c01fc4547c7892047882cf6c9
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-05 10:24:54 +0800

    feed detail

--- src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
@@ -1,8 +1,11 @@
 package cn.momia.mapi.api.v1.feed;
 
+import cn.momia.api.course.CourseServiceApi;
+import cn.momia.api.course.dto.CourseDto;
 import cn.momia.api.feed.FeedServiceApi;
 import cn.momia.api.feed.dto.FeedCommentDto;
 import cn.momia.api.feed.dto.FeedDto;
+import cn.momia.api.feed.dto.FeedStarDto;
 import cn.momia.api.user.UserServiceApi;
 import cn.momia.api.user.dto.UserDto;
 import cn.momia.common.api.dto.PagedList;
@@ -10,6 +13,7 @@ import cn.momia.common.api.http.MomiaHttpResponse;
 import cn.momia.common.webapp.config.Configuration;
 import cn.momia.image.api.ImageFile;
 import cn.momia.mapi.api.v1.AbstractV1Api;
+import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +26,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/feed")
 public class FeedV1Api extends AbstractV1Api {
+    @Autowired private CourseServiceApi courseServiceApi;
     @Autowired private FeedServiceApi feedServiceApi;
     @Autowired private UserServiceApi userServiceApi;
 
@@ -55,10 +60,51 @@ public class FeedV1Api extends AbstractV1Api {
 
     private void processFeeds(List<FeedDto> feeds) {
         for (FeedDto feed : feeds) {
-            List<String> imgs = feed.getImgs();
-            feed.setImgs(completeMiddleImgs(imgs));
-            feed.setLargeImgs(completeLargeImgs(imgs));
-            feed.setAvatar(ImageFile.smallUrl(feed.getAvatar()));
+            processFeed(feed);
+        }
+    }
+
+    private void processFeed(FeedDto feed) {
+        List<String> imgs = feed.getImgs();
+        feed.setImgs(completeMiddleImgs(imgs));
+        feed.setLargeImgs(completeLargeImgs(imgs));
+        feed.setAvatar(ImageFile.smallUrl(feed.getAvatar()));
+    }
+
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public MomiaHttpResponse detail(@RequestParam(defaultValue = "") String utoken, @RequestParam long id) {
+        if (id <= 0) return MomiaHttpResponse.BAD_REQUEST;
+
+        long userId = StringUtils.isBlank(utoken) ? 0 : userServiceApi.get(utoken).getId();
+        FeedDto feed = feedServiceApi.get(userId, id);
+        processFeed(feed);
+
+        PagedList<FeedStarDto> stars = feedServiceApi.listStars(id, 0, Configuration.getInt("PageSize.FeedDetailStar"));
+        processStars(stars.getList());
+        PagedList<FeedCommentDto> comments = feedServiceApi.listComments(id, 0, Configuration.getInt("PageSize.FeedDetailComment"));
+        processComments(comments.getList());
+
+        CourseDto course = courseServiceApi.get(feed.getCourseId());
+        course.setCover(ImageFile.largeUrl(course.getCover()));
+
+        JSONObject feedDetailJson = new JSONObject();
+        feedDetailJson.put("feed", feed);
+        feedDetailJson.put("staredUsers", stars);
+        feedDetailJson.put("comments", comments);
+        feedDetailJson.put("course", course);
+
+        return MomiaHttpResponse.SUCCESS(feedDetailJson);
+    }
+
+    private void processStars(List<FeedStarDto> stars) {
+        for (FeedStarDto star : stars) {
+            star.setAvatar(ImageFile.smallUrl(star.getAvatar()));
+        }
+    }
+
+    private void processComments(List<FeedCommentDto> comments) {
+        for (FeedCommentDto comment : comments) {
+            comment.setAvatar(ImageFile.smallUrl(comment.getAvatar()));
         }
     }
 
@@ -81,12 +127,6 @@ public class FeedV1Api extends AbstractV1Api {
         processComments(pagedComments.getList());
 
         return MomiaHttpResponse.SUCCESS(pagedComments);
-    }
-
-    private void processComments(List<FeedCommentDto> comments) {
-        for (FeedCommentDto comment : comments) {
-            comment.setAvatar(ImageFile.smallUrl(comment.getAvatar()));
-        }
     }
 
     @RequestMapping(value = "/comment/add", method = RequestMethod.POST)
