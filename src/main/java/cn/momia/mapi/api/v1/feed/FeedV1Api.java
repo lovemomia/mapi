cf3fa08d4af0b118f2606c35d598e1ab3354f760
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-10 14:29:15 +0800

    评论相关限制

--- src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
+++ src/main/java/cn/momia/mapi/api/v1/feed/FeedV1Api.java
@@ -98,7 +98,7 @@ public class FeedV1Api extends AbstractV1Api {
         CourseDto course = courseServiceApi.get(feedJson.getLong("courseId"), CourseDto.Type.BASE);
         feedJson.put("subjectId", course.getSubjectId());
 
-        if (!courseServiceApi.finished(user.getId(), feedJson.getLong("courseId"))) return MomiaHttpResponse.FAILED("发表Feed失败，所选课程不存在或您还没参加该课程");
+        if (!courseServiceApi.joined(user.getId(), feedJson.getLong("courseId"))) return MomiaHttpResponse.FAILED("发表Feed失败，所选课程不存在或您还没参加该课程");
         feedServiceApi.add(feedJson);
 
         return MomiaHttpResponse.SUCCESS;
