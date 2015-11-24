bfa785e6ec6e801f74aa0ddd0331b10aed4d5634
吴秋锋 <arthas.wuqf@gmail.com>
2015-11-24 10:38:00 +0800

    去掉VersionController

--- src/main/java/cn/momia/mapi/web/ctrl/MApiController.java
+++ src/main/java/cn/momia/mapi/web/ctrl/MApiController.java
@@ -1,21 +1,30 @@
 package cn.momia.mapi.web.ctrl;
 
+import cn.momia.common.webapp.ctrl.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 @Controller
-public class MApiController extends VersionController {
+public class MApiController extends BaseController {
+    private static final Pattern PATTERN = Pattern.compile("^/v\\d+/.+");
+
     @RequestMapping(value = "/m/**", method = { RequestMethod.GET, RequestMethod.POST })
     public String processMRequest(HttpServletRequest request) {
         return forward(request, request.getRequestURI().substring(2));
     }
 
+    public String forward(HttpServletRequest request, String uri) {
+        if (PATTERN.matcher(uri).find()) return "forward:" + uri;
+        return "forward:/v1" + uri;
+    }
+
     @RequestMapping(value = "/**", method = { RequestMethod.GET, RequestMethod.POST })
     public String processRequest(HttpServletRequest request) {
-        return forward(request);
+        return forward(request, request.getRequestURI());
     }
 
     @RequestMapping(value = "/v{\\d+}/**", method = { RequestMethod.GET, RequestMethod.POST })
