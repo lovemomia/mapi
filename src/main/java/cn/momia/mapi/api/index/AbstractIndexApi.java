2dfad8291cc009fbfd11d1789a82370ae662b019
吴秋锋 <arthas.wuqf@gmail.com>
2016-02-17 11:13:54 +0800

    refactor operator

--- src/main/java/cn/momia/mapi/api/index/AbstractIndexApi.java
+++ src/main/java/cn/momia/mapi/api/index/AbstractIndexApi.java
@@ -1,9 +1,7 @@
 package cn.momia.mapi.api.index;
 
 import cn.momia.api.operate.ConfigServiceApi;
-import cn.momia.api.operate.dto.Banner;
-import cn.momia.api.operate.dto.Event;
-import cn.momia.api.operate.dto.Icon;
+import cn.momia.api.operate.dto.Config;
 import cn.momia.common.webapp.config.Configuration;
 import cn.momia.mapi.api.AbstractApi;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +12,10 @@ import java.util.List;
 public class AbstractIndexApi extends AbstractApi {
     @Autowired private ConfigServiceApi configServiceApi;
 
-    protected List<Banner> getBanners(int cityId, int platform, String clientVersion) {
-        List<Banner> banners = configServiceApi.listBanners(cityId);
-        List<Banner> filteredBanners = new ArrayList<Banner>();
-        for (Banner banner : banners) {
+    protected List<Config.Banner> getBanners(int cityId, int platform, String clientVersion) {
+        List<Config.Banner> banners = configServiceApi.listBanners(cityId);
+        List<Config.Banner> filteredBanners = new ArrayList<Config.Banner>();
+        for (Config.Banner banner : banners) {
             if (banner.isInvalid(platform, clientVersion)) continue;
             banner.setCover(completeImg(banner.getCover()));
             banner.setAction(buildAction(banner.getAction(), platform));
@@ -28,10 +26,10 @@ public class AbstractIndexApi extends AbstractApi {
         return filteredBanners.size() > maxCount ? filteredBanners.subList(0, maxCount) : filteredBanners;
     }
 
-    protected List<Icon> getIcons(int cityId, int platform, String clientVersion) {
-        List<Icon> icons = configServiceApi.listIcons(cityId);
-        List<Icon> filteredIcons = new ArrayList<Icon>();
-        for (Icon icon : icons) {
+    protected List<Config.Icon> getIcons(int cityId, int platform, String clientVersion) {
+        List<Config.Icon> icons = configServiceApi.listIcons(cityId);
+        List<Config.Icon> filteredIcons = new ArrayList<Config.Icon>();
+        for (Config.Icon icon : icons) {
             if (icon.isInvalid(platform, clientVersion)) continue;
             icon.setImg(completeImg(icon.getImg()));
             icon.setAction(buildAction(icon.getAction(), platform));
@@ -42,10 +40,10 @@ public class AbstractIndexApi extends AbstractApi {
         return filteredIcons.size() > maxCount ? filteredIcons.subList(0, maxCount) : filteredIcons;
     }
 
-    protected List<Event> getEvents(int cityId, int platform, String clientVersion) {
-        List<Event> events = configServiceApi.listEvents(cityId);
-        List<Event> filteredEvents = new ArrayList<Event>();
-        for (Event event : events) {
+    protected List<Config.Event> getEvents(int cityId, int platform, String clientVersion) {
+        List<Config.Event> events = configServiceApi.listEvents(cityId);
+        List<Config.Event> filteredEvents = new ArrayList<Config.Event>();
+        for (Config.Event event : events) {
             if (event.isInvalid(platform, clientVersion)) continue;
             event.setImg(completeImg(event.getImg()));
             event.setAction(buildAction(event.getAction(), platform));
