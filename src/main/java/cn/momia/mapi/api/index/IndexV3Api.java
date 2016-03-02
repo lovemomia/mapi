f472d079dcbe8224bdb02bef59c9b2c616cfcac4
吴秋锋 <arthas.wuqf@gmail.com>
2016-03-02 18:06:31 +0800

    discuss topic 接口调整

--- src/main/java/cn/momia/mapi/api/index/IndexV3Api.java
+++ src/main/java/cn/momia/mapi/api/index/IndexV3Api.java
@@ -6,10 +6,13 @@ import cn.momia.api.course.dto.course.Course;
 import cn.momia.api.course.dto.subject.Subject;
 import cn.momia.api.discuss.DiscussServiceApi;
 import cn.momia.api.discuss.dto.DiscussTopic;
+import cn.momia.api.user.UserServiceApi;
+import cn.momia.api.user.dto.User;
 import cn.momia.common.core.dto.PagedList;
 import cn.momia.common.core.http.MomiaHttpResponse;
 import cn.momia.common.webapp.config.Configuration;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +32,11 @@ public class IndexV3Api extends AbstractIndexApi {
     @Autowired private CourseServiceApi courseServiceApi;
     @Autowired private SubjectServiceApi subjectServiceApi;
     @Autowired private DiscussServiceApi discussServiceApi;
+    @Autowired private UserServiceApi userServiceApi;
 
     @RequestMapping(method = RequestMethod.GET)
     public MomiaHttpResponse index(HttpServletRequest request,
+                                   @RequestParam(required = false, defaultValue = "") String utoken,
                                    @RequestParam(value = "city") int cityId,
                                    @RequestParam int start) {
         if (cityId < 0) return MomiaHttpResponse.FAILED("无效的CityID");
@@ -43,19 +48,34 @@ public class IndexV3Api extends AbstractIndexApi {
             String version = getVersion(request);
 
             indexJson.put("banners", getBanners(cityId, platform, version));
-            indexJson.put("subjects", getSubjects(cityId));
-
-            PagedList<DiscussTopic> topics = discussServiceApi.listTopics(cityId, 0, 1);
-            if (topics.getList() != null && !topics.getList().isEmpty()) {
-                DiscussTopic topic = topics.getList().get(0);
-                topic.setCover(completeLargeImg(topic.getCover()));
-                indexJson.put("topic", topic);
+            if (!StringUtils.isBlank(utoken)) {
+                User user = userServiceApi.get(utoken);
+                if (!user.isPayed()) indexJson.put("trials", getTrials(cityId));
+            } else {
+                indexJson.put("trials", getTrials(cityId));
             }
+
+            List<Subject> subjects = getSubjects(cityId);
+            // TODO courses
+            indexJson.put("subjects", subjects);
+
+            List<DiscussTopic> topics = discussServiceApi.listTopics(cityId, 0, 3).getList();
+            for (DiscussTopic topic : topics) {
+                topic.setCover(completeLargeImg(topic.getCover()));
+            }
+            indexJson.put("topics", topics);
         }
 
         indexJson.put("courses", getRecommendCourses(cityId, start));
 
         return MomiaHttpResponse.SUCCESS(indexJson);
+    }
+
+    private PagedList<Course> getTrials(int cityId) {
+        PagedList<Course> courses = courseServiceApi.listTrial(cityId, 0, Configuration.getInt("PageSize.Trial"));
+        completeLargeCoursesImgs(courses.getList());
+
+        return courses;
     }
 
     private List<Subject> getSubjects(int cityId) {
