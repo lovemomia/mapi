d87112f5d54a598e2fbaa2bb70fe742d6c797486
吴秋锋 <arthas.wuqf@gmail.com>
2016-03-10 14:48:05 +0800

    fix bug

--- src/main/java/cn/momia/mapi/api/index/ConfigService.java
+++ src/main/java/cn/momia/mapi/api/index/ConfigService.java
@@ -16,7 +16,7 @@ public class ConfigService extends AbstractService {
     }
 
     public List<Config.Event> listEvents(int cityId) {
-        String sql = "SELECT Title, Img, `Desc`, Action, Platform, Version FROM SG_Event WHERE (CityId=? OR CityId=0) AND Status=1 ORDER BY Weight DESC, AddTime DESC";
+        String sql = "SELECT `Type`, Title, Img, `Desc`, Action, Platform, Version FROM SG_Event WHERE (CityId=? OR CityId=0) AND Status=1 ORDER BY Weight DESC, AddTime DESC";
         return queryObjectList(sql, new Object[] { cityId }, Config.Event.class);
     }
 }
